e6367f6c1202fed6db97c36130034793740a4881
Archer-SN <superkakanoob@gmail.com>
2023-04-11 18:48:08 +0700

    Wrote a skeleton code for ArrayDeque and ArrayDequeTest

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,101 @@
 /* A circular array class */
 public class ArrayDeque<T> {
+    int defaultSize = 8;
+    int defaultFactor = 2;
 
+    T[] items;
+    int size;
+    int nextFirst;
+    int nextLast;
+
+    private int currentFirst() {
+
+    }
+
+    private int currentLast() {
+
+    }
+
+    /* Creates an empty array with default size */
+    public ArrayDeque() {
+        items = (T[]) new Object[defaultSize];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    private void resize(int factor) {
+
+    }
+
+    /* Returns a number subtracted by 1 */
+    private int minusOne(int number) {
+        return number - 1;
+    }
+
+    /* Returns a number added by one */
+    private int addOne(int number) {
+        return number + 1;
+    }
+
+    /* Moves nextFirst index by i */
+    private void moveNextFirst(int i) {
+        nextFirst += i;
+        if (nextFirst == -1) {
+            nextFirst = minusOne(items.length);
+        }
+        else if (nextFirst == items.length) {
+            nextFirst = 0;
+        }
+    }
+
+    /* Moves nextLast index by i */
+    private void moveNextLast(int i) {
+        nextLast += i;
+        if (nextLast == -1) {
+            nextLast = minusOne(items.length);
+        }
+        else if (nextLast == items.length) {
+            nextLast = 0;
+        }
+    }
+
+
+    /* Adds an item to the front of the array deque
+    *  (Not the front of items) */
+    public void addFirst(T item) {
+        items[nextFirst] = item;
+        nextFirst -= 1;
+
+    }
+
+    /* Adds an item to the last of the array deque
+     *  (Not the last of items) */
+    public void addLast(T item) {
+
+    }
+
+    boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+
+    }
+
+    public T removeFirst() {
+
+    }
+
+    public T removeLast() {
+
+    }
+
+    public T get(int index) {
+        return items[minusOne(nextFirst) + index]
+    }
 }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -1,3 +1,61 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+
 public class ArrayDequeTest {
 
+    @Test
+    public void getTest() {
+        int expected = 1; // Expected return value
+        ArrayDeque<Integer> ad = new ArrayDeque<Integer>();
+        ad.addFirst(1);
+        ad.addFirst(2);
+        ad.addFirst(3);
+        ad.addLast(4);
+        int actual = ad.get(2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addFirstTest() {
+
+    }
+
+    @Test
+    public void addLastTest() {
+
+
+    }
+
+    @Test
+    public void isEmptyTest() {
+
+    }
+
+    @Test
+    public void sizeTest() {
+    }
+
+    @Test
+    public void printDequeTest() {
+
+    }
+
+    @Test
+    public void removeFirstTest() {
+
+    }
+
+    @Test
+    public void removeLastTest() {
+
+    }
+
+    @Test
+    public void resizeTest() {
+
+    }
 }
