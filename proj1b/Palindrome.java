f458cd5127cbbf61d9d4caf46cb1d88e33ffb9b9
Archer-SN <superkakanoob@gmail.com>
2023-04-13 13:45:53 +0700

    Finished Palindrome and is working on OffByOne

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,2 +1,27 @@
 public class Palindrome {
+
+    /** Given String, convert it to Deque */
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> deque = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+        return deque;
+    }
+
+    /** Checks if a given word is a palindrome */
+    public boolean isPalindrome(String word) {
+        Deque<Character> deque = wordToDeque(word);
+        while (deque.size() > 0) {
+            /* Handles cases with odd length */
+            if (deque.size() == 1) {
+                return true;
+            }
+            /* If the first is not equal to last then it is not a palindrome */
+            else if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
