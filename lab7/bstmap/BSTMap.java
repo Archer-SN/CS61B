d45843d86f49252281d14829317ac18a4d7ef716
Archer-SN <superkakanoob@gmail.com>
2023-05-10 13:08:23 +0700

    Implemented keySet and iterator method for BSTMap.java. remove method is still in progress

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -4,12 +4,17 @@ import org.w3c.dom.Node;
 
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 /* This data structure is Map implemented using Binary Search Tree
  * Each key-value pair is stored as a node in the Binary Search Tree
  *
  * */
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    /**
+     * A node in BST
+     */
     private class BSTNode {
         public K key;
         public V value;
@@ -95,7 +100,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /**
      * Recursively find the value that is associated with the given key
      */
-    private V get(K key, BSTMap<K, V> T) {
+    private BSTMap<K, V> get(K key, BSTMap<K, V> T) {
         // Base case
         if (T == null || T.node == null) {
             return null;
@@ -105,7 +110,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
         // If key == node.key
         if (comparison == 0) {
-            return T.node.value;
+            return T;
         }
         // if key < node.key
         else if (comparison < 0) {
@@ -122,7 +127,11 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        return get(key, this);
+        BSTMap<K, V> T = get(key, this);
+        if (T == null) {
+            return null;
+        }
+        return T.node.value;
     }
 
     @Override
@@ -167,14 +176,85 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         size += 1;
     }
 
-    @Override
-    public Set<K> keySet() {
-        throw new UnsupportedOperationException("keySet not supported!");
+
+    /**
+     * Gets all the keys in a tree recursively
+     * We make use of mutability property of Set
+     */
+    private Set<K> keySet(Set<K> S, BSTMap<K, V> T) {
+        if (T == null) {
+            return S;
+        }
+        keySet(S, T.left);
+        S.add(T.node.key);
+        keySet(S, T.right);
+        return S;
     }
 
+    /**
+     * Returns a set view of keys stored in the BSTMap
+     */
+    @Override
+    public Set<K> keySet() {
+        return keySet(new TreeSet<K>(), this);
+    }
+
+
+    /** Given a key returns the key's parent tree
+     * The existence of the key is assumed to exist */
+    private BSTMap<K, V> getParentNode(BSTMap<K, V> currentT, BSTMap<K, V> parentT, K key) {
+        int comparison = key.compareTo(currentT.node.key);
+        // key == node.key
+        if (comparison == 0) {
+            return parentT;
+        }
+        // key < node.key
+        else if (comparison < 0) {
+            return getParentNode(currentT.left, currentT, key);
+        }
+        else {
+            return getParentNode(currentT.right, currentT, key);
+        }
+    }
+
+    private void getRightMostNode()
+
+    /**
+     * Remove the mapping for the specified key if it is present
+     */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException("remove not supported!");
+        BSTMap<K, V> T = get(key, this);
+        BSTMap<K,V> parentT = getParentNode(this, this, key);
+        if (T == null) {
+            return null;
+        }
+        else if (T.left == null && T.right == null) {
+            V value = T.node.value;
+            T = null;
+            return value;
+        }
+        else if (T.left != null && T.right != null) {
+
+        }
+        else {
+            BSTMap<K, V> leaf;
+            // Stores the leaf value before we remove the current node
+            if (T.left == null) {
+                leaf = T.right;
+            }
+            else {
+                leaf = T.left;
+            }
+
+            // Relink the parent node with leaf
+            if (parentT.right == T) {
+                parentT.right = leaf;
+            }
+            else {
+                parentT.left = leaf;
+            }
+        }
     }
 
     @Override
@@ -182,8 +262,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         throw new UnsupportedOperationException("remove not supported!");
     }
 
+
+    /**
+     * Returns an iterator for BSTMap
+     * Implemented using set iterator
+     */
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException("iterator not supported!");
+        return keySet().iterator();
     }
 }
