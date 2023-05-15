fac4c07978042e2164cbf8128b20cc9f55d94c35
Archer-SN <superkakanoob@gmail.com>
2023-05-15 19:43:08 +0700

    Given some structure to Main.java

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,24 +1,62 @@
 package gitlet;
 
-/** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
+/**
+ * Driver class for Gitlet, a subset of the Git version-control system.
+ *
+ * @author TODO
  */
 public class Main {
 
-    /** Usage: java gitlet.Main ARGS, where ARGS contains
-     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+    /**
+     * Usage: java gitlet.Main ARGS, where ARGS contains
+     * <COMMAND> <OPERAND1> <OPERAND2> ...
      */
     public static void main(String[] args) {
         // TODO: what if args is empty?
+        if (args.length == 0) {
+            throw new GitletException("Not enough argument!");
+        }
         String firstArg = args[0];
-        switch(firstArg) {
+        switch (firstArg) {
             case "init":
                 // TODO: handle the `init` command
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
-            // TODO: FILL THE REST IN
+            case "commit":
+                // TODO
+                break;
+            case "rm":
+                // TODO
+                break;
+            case "log":
+                // TODO
+                break;
+            case "global-log":
+                // TODO
+                break;
+            case "find":
+                // TODO
+                break;
+            case "status":
+                // TODO
+                break;
+            case "checkout":
+                // TODO
+                break;
+            case "branch":
+                // TODO
+                break;
+            case "rm-branch":
+                // TODO
+                break;
+            case "reset":
+                // TODO
+                break;
+            case "merge":
+                // TODO
+                break;
         }
     }
 }
